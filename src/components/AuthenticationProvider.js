abbb2bd1449ec5f9b3a05054ae7e816a4d971908
zipang <christophe.Desguez@gmail.com>
2020-12-17 17:58:46 +0100

    withAuthentication : HOC pour interdire l'accès aux pages privées (espace membre, tableaux de bord..) sans login

--- src/components/AuthenticationProvider.js
+++ src/components/AuthenticationProvider.js
@@ -1,4 +1,6 @@
-import { useState, useContext, createContext } from "react";
+import { useState, useContext, createContext, useEffect } from "react";
+import { useRouter } from "next/router";
+import User from "@models/User";
 
 const AuthContext = createContext();
 
@@ -31,4 +33,47 @@ export const useAuthentication = () => {
 		);
 	}
 	return auth;
+};
+
+/**
+ * @typedef AuthenticationOptions
+ * @field {String[]} profiles check that the logged user has at least one of these profiles
+ * @param {String} [redirectTo="/"] path to redirect if no logged user is found
+ */
+/**
+ * Build a HOC to pass which we pass the logged user or redirect
+ * @param {JSX.Element} Component
+ * @param {AuthenticationOptions} options
+ */
+export const withAuthentication = (Component, { profiles = [], redirectTo = "/" }) => ({
+	...props
+}) => {
+	const { loggedUser } = useContext(AuthContext);
+	const router = useRouter();
+
+	useEffect(() => {
+		if (!loggedUser) {
+			// not logged
+			console.log(`No logged user. Redirect to ${redirectTo}`);
+			router.replace(redirectTo);
+		} else if (
+			profiles.length &&
+			!profiles.find((profile) => loggedUser.profiles.includes(profile))
+		) {
+			// bad profile
+			console.log(
+				`Bad profile (we looked for ${profiles}). Redirect to ${redirectTo}`
+			);
+			router.replace(redirectTo);
+		}
+
+		return () => {};
+	}, [loggedUser]);
+
+	if (typeof window === "undefined") {
+		console.log("No SSR");
+		return null;
+	} else {
+		return <Component user={loggedUser} {...props} />;
+	}
 };
