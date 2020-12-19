dbb370d7740e82ce8bdbe50c6968a917817090b0
zipang <christophe.Desguez@gmail.com>
2020-12-19 03:41:26 +0100

    Use NoSSR (is it better ?)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,29 @@
-import React from "react";
-import PropTypes from "prop-types";
+import { useEffect } from "react";
 import AuthenticationProvider from "@components/AuthenticationProvider";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../theme";
+import { NoSsr } from "@material-ui/core";
 
-const MyApp = ({ Component, pageProps }) => (
-	<AuthenticationProvider>
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<Component {...pageProps} />
-		</ThemeProvider>
-	</AuthenticationProvider>
-);
+const MyApp = ({ Component, pageProps }) => {
+	// useEffect(() => {
+	// 	// Remove the server-side injected CSS.
+	// 	const jssStyles = document.querySelector("#jss-server-side");
+	// 	if (jssStyles) {
+	// 		jssStyles.parentElement.removeChild(jssStyles);
+	// 	}
+	// }, []);
 
-MyApp.propTypes = {
-	Component: PropTypes.elementType.isRequired,
-	pageProps: PropTypes.object.isRequired
+	return (
+		<AuthenticationProvider>
+			<NoSsr>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</NoSsr>
+		</AuthenticationProvider>
+	);
 };
 
 export default MyApp;
