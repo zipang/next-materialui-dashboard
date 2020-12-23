562568d2f2f5456d5793fffa740dd8f9bd6df1a8
zipang <christophe.Desguez@gmail.com>
2020-12-23 22:49:33 +0100

    Still a work in progress..

--- src/components/StateMachine.js
+++ src/components/StateMachine.js
@@ -4,36 +4,66 @@ import { deepMerge } from "@lib/utils/deepMerge";
 const StateMachinesContext = createContext();
 
 /**
+ * Utility on which more complex actions can rely
+ */
+const _DEFAULT_ACTIONS = {
+	merge: (state, payload) => deepMerge({}, state, payload),
+	overwrite: (state, payload) => ({ ...payload })
+};
+
+/**
  * @typedef StateMachineDef
  * @field {String} id a unique name for this state machine
- * @field {Object} [state={}] the initial state
+ * @field {Object} [initialState={}] the initial state
  * @field {Object} actions an objects where keys are action names
  *                 and values are functions that return a new state
  * @field {Array} [midlewares=[]] an optional array of function to be runned each times the state change
  */
-/**
- * @param {StateMachineDef} machineDef
- */
-const createMachine = ({ id, state = {}, actions = {} }) => {
-	// Transform the actions so that they really change the state
-	Object.keys(actions).forEach((actionName) => {
-		action = (payload) => () => {};
-	});
+
+export const useStateMachine = () => {
+	const sm = useContext(StateMachinesContext);
+	if (!sm) {
+		throw new Error(
+			`useStateMachine() hook can only be used from inside a decorated withStateMachine() HOC`
+		);
+	}
+	return sm;
 };
 
-const useMachine = () => {};
 /**
  *
  * @param {JSX.Element} Component
  * @param {StateMachineDef} stateMachineDef
  */
-const withStateMachine = (Component, stateMachineDef) => ({ ...props }) => {
-	const [stateMachine, setStateMachine] = useState();
+const withStateMachine = (
+	Component,
+	{ id = "state-machine", initialState = {}, actions = {}, middlewares = [] }
+) => ({ ...props }) => {
+	const [state, setState] = useState(initialState);
 
-	setStateMachine(createMachine(machineDef, setStateMachine));
+	if (!actions.__machineEnabled) {
+		// Wrap the actions with a setState and middleware
+		Object.keys(actions).forEach((name) => {
+			const originalAction = actions[name];
+			actions[name] = ([...args]) => {
+				const newState = originalAction(state, ...args);
+				if (newState !== undefined && newState !== state) {
+					console.log(
+						`Action ${name} will transition ${id} state to ${JSON.stringify(
+							newState,
+							null,
+							"\t"
+						)}`
+					);
+					setState(newState);
+				}
+			};
+		});
+		actions.__machineEnabled = true;
+	}
 
 	return (
-		<StateMachinesContext.Provider value={stateMachine}>
+		<StateMachinesContext.Provider value={{ state, actions }}>
 			<Component {...props} />
 		</StateMachinesContext.Provider>
 	);
