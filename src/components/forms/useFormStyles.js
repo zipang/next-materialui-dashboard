5a9e2ad8669dd0984ba91fd98de4069ce7269539
zipang <christophe.Desguez@gmail.com>
2020-12-23 22:43:03 +0100

    Default form styles

---
+++ src/components/forms/useFormStyles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+/**
+ * Create specific css class names for our form
+ */
+const useFormStyles = makeStyles((theme) => ({
+	form: {
+		height: "100%",
+		width: "100%",
+		padding: "1rem",
+		marginTop: theme.spacing(1),
+		alignItems: "center",
+		justify: "center",
+		"& input": {
+			fontWeight: 800
+		},
+		"& .submit": {
+			margin: theme.spacing(3, 0, 2)
+		}
+	}
+}));
+
+export default useFormStyles;
