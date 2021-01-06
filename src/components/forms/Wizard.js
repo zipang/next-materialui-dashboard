aceae96843da004883b26644c5ad56e3d664a8d6
zipang <christophe.Desguez@gmail.com>
2021-01-06 17:01:02 +0100

    Remove dead code

--- src/components/forms/Wizard.js
+++ src/components/forms/Wizard.js
@@ -1,15 +1,11 @@
 import { forwardRef, useEffect, memo } from "react";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
-import Slide from "@material-ui/core/Slide";
 import { makeStyles } from "@material-ui/core/styles";
 
 import SvgIcon from "@components/SvgIcon";
 import { withStateMachine } from "@components/StateMachine";
-import EventBusProvider, {
-	useEventBus,
-	withEventBus
-} from "@components/EventBusProvider";
+import { useEventBus, withEventBus } from "@components/EventBusProvider";
 
 /**
  * @typedef Step
@@ -46,37 +42,29 @@ const WizardViewport = ({ children }) => (
  * Being used inside a Slide component, the Display Step must forward a ref
  * @see https://material-ui.com/guides/composition/#caveat-with-refs
  */
-const DisplayStep = memo(
-	forwardRef(({ step, data, onSubmit }, ref) => (
-		<Box width="100%" height="100%" ref={ref}>
-			<h2>{step.title}</h2>
-			{step.displayForm(data, onSubmit)}
-		</Box>
-	))
+const DisplayStep = ({ step, data, onSubmit }) => (
+	<Box width="100%" height="100%">
+		<h2>{step.title}</h2>
+		{step.displayForm(data, onSubmit)}
+	</Box>
 );
-
-const WizardControls = ({ children }) => (
+const WizardControls = memo(({ children }) => (
 	<Box display="flex" flexDirection="row" justifyContent="flex-end" padding="1rem">
 		{children}
 	</Box>
-);
+));
 
 /**
  * Define the actions that are available on our Wizard
  * Finite State Machine associates a given state
  * and a defined set of actions to transition from one state to another
  * @param {Object} state Initial state available to any actions
- * @param {Function} setState Apply the new state and re-render the component
  */
 
 const transition = (state, to) => {
-	const [slideDirectionIn, slideDirectionOut] =
-		state.currentSlide < to ? ["right", "left"] : ["left", "right"];
 	return {
 		...state,
-		currentSlide: to,
-		slideDirectionIn,
-		slideDirectionOut
+		currentSlide: to
 	};
 };
 const next = (state) => {
@@ -93,12 +81,11 @@ const InitWizard = ({ steps = [], state, actions }) => {
 	const eb = useEventBus();
 
 	// flatten the state
-	const { data, currentSlide, slideDirectionIn, slideDirectionOut } = state;
+	const { data, currentSlide } = state;
 
 	// Define a validate combo action that merge step data and transition to next step
 	const validateStep = (payload) => {
 		console.dir(`Validate step payload :`, payload);
-		console.trace();
 		actions.merge(state, { data: payload });
 		actions.next();
 	};
@@ -122,19 +109,6 @@ const InitWizard = ({ steps = [], state, actions }) => {
 					data={data}
 					onSubmit={validateStep}
 				/>
-				{/* {steps.map((step, i) => ( 
-					// <Slide
-					// 	key={`slide-${i}`}
-					// 	in={i === currentSlide}
-					// 	direction={
-					// 		i === currentSlide ? slideDirectionOut : slideDirectionIn
-					// 	}
-					// 	mountOnEnter
-					// 	unmountOnExit
-					// >
-					// 	<DisplayStep step={step} data={data} onSubmit={validateStep} />
-					// </Slide>
-				// ))}*/}
 			</WizardViewport>
 			<WizardControls>
 				<Button
@@ -146,7 +120,6 @@ const InitWizard = ({ steps = [], state, actions }) => {
 				</Button>
 				<Button
 					variant="outlined"
-					ml="1rem"
 					disabled={currentSlide >= steps.length - 1}
 					onClick={actions.next}
 				>
@@ -155,7 +128,6 @@ const InitWizard = ({ steps = [], state, actions }) => {
 				<Button
 					variant="contained"
 					color="secondary"
-					ml="1rem"
 					startIcon={<SvgIcon name="Save" />}
 					onClick={triggerValidate}
 				>
@@ -170,9 +142,7 @@ InitWizard.StateMachine = {
 	useLocalStorage: true,
 	initialState: {
 		data: {},
-		currentSlide: 0,
-		slideDirectionIn: "right",
-		slideDirectionOut: "left"
+		currentSlide: 0
 	},
 	actions: {
 		transition,
