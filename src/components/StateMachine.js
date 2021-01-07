dba2d617cbe9c9e1d1d2f894a22b28e43323835d
zipang <christophe.Desguez@gmail.com>
2021-01-07 22:48:38 +0100

    FIX : merge action + prevent base actions to be overwritten

--- src/components/StateMachine.js
+++ src/components/StateMachine.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useState } from "react";
 import { deepMerge } from "@lib/utils/deepMerge";
 
 const StateMachinesContext = createContext();
@@ -7,18 +7,17 @@ const StateMachinesContext = createContext();
  * Utility action on which more complex actions can rely
  */
 const _DEFAULT_ACTIONS = {
-	merge: (state, payload) => deepMerge(state, payload)
+	merge: deepMerge
 };
 
 const _MIDDLEWARES = {
-	log: (id, actionName, oldState, newState) => {
-		console.dir(
-			`State machine ${id} transition ${actionName} : `,
-			oldState,
-			` => `,
-			newState
-		);
-	},
+	/**
+	 * Save the new state inside the local storage
+	 * @param {String} id
+	 * @param {String} actionName
+	 * @param {Object} oldState
+	 * @param {Object} newState
+	 */
 	localStorage: (id, actionName, oldState, newState) => {
 		if (window && window.localStorage) {
 			window.localStorage.setItem(id, newState);
@@ -45,6 +44,83 @@ export const useStateMachine = () => {
 	return sm;
 };
 
+export const enhanceActions = (id, actions, state, setState, middlewares) => {
+	if (actions._enhanced) {
+		console.log(`Actions are already enhanced !`);
+		return actions;
+	}
+
+	if (actions.transition || actions.merge) {
+		throw new TypeError(
+			"Warning : 'transition' and 'merge' are 2 predefined actions names that are reserved for the state machine."
+		);
+	}
+
+	console.log(`Enhancing state machine actions : ${Object.keys(actions)}`);
+
+	const transition = (...args) => {
+		if (args.length !== 4) {
+			throw new TypeError(
+				"actions.transition() must receive 4 arguments : machineId, actionName, oldState and newState"
+			);
+		} else {
+			const [id, actionName, oldState, newState] = args;
+			console.dir(
+				`State machine ${id} transition '${actionName}' : `,
+				oldState,
+				` => `,
+				newState
+			);
+
+			setState(newState);
+		}
+	};
+
+	const enhancedActions = Object.keys(actions).reduce(
+		(wrapped, name) => {
+			// Create an inactive version that just return the state
+			wrapped[`get_${name}`] = (...args) => actions[name](state, ...args);
+
+			// Create the active version that applies the state transition
+			wrapped[name] = (...args) => {
+				try {
+					let newState = actions[name](state, ...args);
+
+					if (newState !== undefined && newState !== state) {
+						// Apply the middlewares
+						middlewares.forEach((midw) => {
+							const updated = midw(id, name, state, newState);
+							if (typeof updated === "object") {
+								newState = updated;
+							}
+						});
+						transition(id, name, state, newState);
+					} else {
+						console.log(`Action ${name} didn't change ${id} machine state`);
+					}
+				} catch (err) {
+					console.error(
+						`Action ${name}(${JSON.stringify(
+							state,
+							null,
+							"\t"
+						)}) failed on ${id} machine`
+					);
+				}
+			};
+
+			return wrapped;
+		},
+		{
+			transition,
+			..._DEFAULT_ACTIONS
+		}
+	);
+	console.log(`State Machine actions : ${Object.keys(enhancedActions)}`);
+	enhancedActions._enhanced = true; // don't do it multiple times
+	return enhancedActions;
+};
+
 /**
  * HOC to inject {state, actions} and a State Machine Provider around the original Component
  * @param {JSX.Element} Component
@@ -57,13 +133,17 @@ export const withStateMachine = (
 	initialProps = {}
 ) => ({ ...props }) => {
 	// we can override the initial state and middlewares this way
-	const mergedDefinitions = deepMerge({}, Component.StateMachine, overrideMachineDef);
+	const mergedDefinitions = { ...Component.StateMachine, ...overrideMachineDef };
+	// Is it too much like this  ??
+	// const mergedDefinitions = deepMerge({}, Component.StateMachine, overrideMachineDef);
+
 	const {
 		id,
 		actions = {},
 		middlewares = [],
 		useLocalStorage = false
 	} = mergedDefinitions;
+
 	let initialState = mergedDefinitions.initialState || {};
 
 	if (useLocalStorage && window && window.localStorage) {
@@ -79,44 +159,10 @@ export const withStateMachine = (
 
 	const [state, setState] = useState(initialState);
 
-	if (process.env.NODE_ENV !== "production") {
-		middlewares.push(_MIDDLEWARES.log);
-	}
-
 	/**
 	 * Wrap the actions, apply the middlewares and the final setState() to automatically transition
 	 **/
-	const enhancedActions = Object.keys(actions).reduce((wrapped, name) => {
-		wrapped[name] = (...args) => {
-			try {
-				let newState = actions[name](state, ...args);
-
-				if (newState !== undefined && newState !== state) {
-					// Apply the middlewares
-					middlewares.forEach((midw) => {
-						const updated = midw(id, name, state, newState);
-						if (typeof updated === "object") {
-							newState = updated;
-						}
-					});
-					setState(newState);
-				} else {
-					console.log(`Action ${name} didn't change ${id} machine state`);
-				}
-			} catch (err) {
-				console.error(
-					`Action ${name}(${JSON.stringify(
-						state,
-						null,
-						"\t"
-					)}) failed on ${id} machine`
-				);
-			}
-		};
-		return wrapped;
-	}, _DEFAULT_ACTIONS);
-
-	console.log(`State Machine actions : ${Object.keys(enhancedActions)}`);
+	const enhancedActions = enhanceActions(id, actions, state, setState, middlewares);
 
 	return (
 		<StateMachinesContext.Provider value={{ state, actions: enhancedActions }}>
