ff870b7633e2e0ae733d552869ac6338d2408393
zipang <christophe.Desguez@gmail.com>
2021-01-08 18:21:58 +0100

    Test page

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,13 +1,12 @@
 import CenteredPaperSheet from "@components/CenteredPaperSheet";
-import { SiretForm } from "@components/forms/registration/SiretSearch";
-import Wizard from "@components/Wizard";
+import RegistrationWizard from "@components/forms/registration/RegistrationWizard";
 
 const IndexPage = () => {
 	const steps = [1, 2, 3, 4, 5];
 
 	return (
 		<CenteredPaperSheet xs={10} md={8}>
-			<SiretForm onSuccess={(resp) => alert(JSON.stringify(resp))} />
+			<RegistrationWizard />
 		</CenteredPaperSheet>
 	);
 };
