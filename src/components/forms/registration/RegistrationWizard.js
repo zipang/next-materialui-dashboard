723714f10ef131186fba5e974265a5406486c9c5
zipang <christophe.Desguez@gmail.com>
2021-01-08 18:21:44 +0100

    Define the steps with the new approach

--- src/components/forms/registration/RegistrationWizard.js
+++ src/components/forms/registration/RegistrationWizard.js
@@ -1,42 +1,22 @@
 import { SiretSearchForm } from "./SiretSearch";
 import { OrganismeForm } from "./Organisme";
 import Wizard from "@forms/Wizard";
-import { useEventBus } from "@components/EventBusProvider";
-import { useStateMachine } from "@components/StateMachine";
 
-const steps = [];
-
-/**
- * Build the steps that are aware of the state machine context
- * @param {StateMachine} sm
- * @return {Array<Steps>}
- */
-const createSteps = ({ state, actions }) => {
-	const mergeSiretData = (siretData) => {};
-
-	// Send the event to validate the data of this step
-	const validateStep = (step) => () => {
-		const eb = useEventBus();
-		eb.emit(`${step}:validate`);
-	};
-	const mergeStepData = (stepData) => {};
-
-	return [
-		{
-			title: "Recherche de l'organisme par son no de SIRET",
-			displayForm: (data) => <SiretSearchForm onSuccess={mergeSiretData} />
-		},
-		{
-			title: "Saisie de l'organisme",
-			displayForm: (data) => <OrganismeForm data={data} onSubmit={mergeStepData} />,
-			validate: validateStep("organisme")
-		}
-	];
-};
+const steps = [
+	{
+		id: "step-01-siret-search",
+		title: "Recherche de l'organisme par son no de SIRET",
+		displayForm: (data) => <SiretSearchForm onSuccess={mergeSiretData} />
+	},
+	{
+		id: "step-02-organisme-name",
+		title: "Saisie de l'organisme",
+		displayForm: (data, onSubmit) => <OrganismeForm data={data} onSubmit={onSubmit} />
+	}
+];
 
 const RegistrationWizard = () => {
-	const sm = useStateMachine({ id: "registration-wizard" });
-	const steps = createSteps();
-
-	return <Wizard steps={steps} />;
+	return <Wizard id="registration-wizard" steps={steps} />;
 };
+
+export default RegistrationWizard;
