610bfb9788bc11255247f28292d770b5c43f18f0
zipang <christophe.Desguez@gmail.com>
2021-01-19 00:32:40 +0100

    Allow to pass custom styles

--- src/components/forms/useFormStyles.js
+++ src/components/forms/useFormStyles.js
@@ -3,22 +3,30 @@ import { makeStyles } from "@material-ui/core/styles";
 /**
  * Create specific css class names for our form
  */
-const useFormStyles = makeStyles((theme) => ({
-	form: {
-		height: "100%",
-		width: "100%",
-		backgroundColor: theme.palette.background.light,
-		padding: "1rem",
-		marginTop: theme.spacing(1),
-		alignItems: "center",
-		justify: "center",
-		"& input": {
-			fontWeight: 800
-		},
-		"& .submit": {
-			margin: theme.spacing(3, 0, 2)
+const useFormStyles = (customStyles = {}) =>
+	makeStyles((theme) => ({
+		form: {
+			height: "100%",
+			width: "100%",
+			backgroundColor: theme.palette.background.light,
+			padding: "1rem",
+			marginTop: theme.spacing(1),
+			marginLeft: "auto",
+			marginRight: "auto",
+			alignItems: "center",
+			justify: "center",
+			"& input": {
+				fontWeight: 800
+			},
+			"& .submit": {
+				margin: theme.spacing(3, 0, 2)
+			},
+			"& input.hidden": {
+				visibility: "hidden",
+				tabIndex: -1
+			},
+			...customStyles
 		}
-	}
-}));
+	}))();
 
 export default useFormStyles;
