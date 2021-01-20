1363572290642b769b5d72397ce1be5712ff5ebc
zipang <christophe.Desguez@gmail.com>
2021-01-20 11:31:22 +0100

    Added support for the Help section

--- src/components/forms/Step.js
+++ src/components/forms/Step.js
@@ -1,15 +1,23 @@
-import { Grid } from "@material-ui/core";
+import { Box, Grid } from "@material-ui/core";
 import Input from "@forms/Input";
 import StepForm from "./StepForm";
 import GroupLabel from "./GroupLabel";
+import { useEventBus } from "@components/EventBusProvider";
+import { useEffect } from "react";
 
 /**
  * @typedef StepDef
  * @param {String} id
  * @param {String} title
+ * @param {HelpDef} [help]
+ * @param {Array<FieldDef>} [fields]
+ * @param {Function} [displayForm] a custom function to display the form without fields
+ */
+
+/**
+ * @typedef HelpDef
  * @param {String} description
- * @param {Number} size
- * @param {Array<FieldDef>} fields
+ * @param {String} [backgroundImage]
  */
 
 /**
@@ -27,26 +35,62 @@ import GroupLabel from "./GroupLabel";
  * From a flat description of the required fields
  * @param {StepDef} step
  */
-function Step(step) {
+function Step({ id, title, help = false, fields = false, displayForm = false }) {
 	// Validate that all required fields are provided
-	["id", "title"].forEach((fieldName) => {
-		if (!step[fieldName]) {
-			throw new TypeError(`A '${fieldName}' is required to create a Step`);
-		}
-	});
-	if (!Array.isArray(step.fields) && typeof step.displayForm !== "function") {
+	if (!id || !title) {
 		throw new TypeError(
-			`You must provide an array of fields definitions or a custom displayForm method to create a Step : 
-			${JSON.stringify(step, null, "\t")}`
+			`The 'id' and 'title' fields are both required to create a Step`
 		);
 	}
-	step.size = step.size || 1;
+	if (!help && !fields && typeof displayForm !== "function") {
+		throw new TypeError(
+			`You must provide a help section or an array of fields definitions or a custom displayForm method to create the Step ${title}`
+		);
+	}
 	// It's ok : assign all
-	Object.assign(this, step);
+	Object.assign(this, { id, title, help, fields });
+	if (typeof displayForm === "function") {
+		this.displayForm = displayForm.bind(this);
+	} else if (!fields) {
+		// Unable to display anything
+		this.displayForm = false;
+	}
 }
 
 Step.prototype = {
+	getBackgroundImageStyle: function () {
+		const backgroundImage = this.help?.backgroundImage;
+		if (!backgroundImage) {
+			return {};
+		} else if (backgroundImage.startsWith("http")) {
+			return { backgroundImage: `url(${backgroundImage})` };
+		} else {
+			return { backgroundImage: `url(/img/${backgroundImage})` };
+		}
+	},
+	displayHelp: function (data, errors, onSubmit) {
+		if (!this.help) return null;
+		const eb = useEventBus();
+		const { description } = this.help;
+		useEffect(() => {
+			if (!this.displayForm) {
+				const validate = () => onSubmit({});
+				eb.on(`${this.id}:validate`, validate);
+				return () => eb.off(`${this.id}:validate`, validate);
+			}
+		});
+		return (
+			<div>
+				<h2>{this.title}</h2>
+				<p>{description}</p>
+			</div>
+		);
+	},
 	displayForm: function (data, onSubmit) {
+		if (!this.fields) {
+			console.log(`Step ${this.id} has no form to display`);
+			return null;
+		}
 		return (
 			<StepForm
 				formId={`${this.id}`}
@@ -54,6 +98,7 @@ Step.prototype = {
 				onSubmit={onSubmit}
 				rerender={new Date()}
 			>
+				{!this.help && <h2>{this.description}</h2>}
 				<Grid container>{this.displayFields(this.fields)}</Grid>
 			</StepForm>
 		);
@@ -67,7 +112,7 @@ Step.prototype = {
 				default:
 					const { type = "text", size = 1, ...fieldProps } = field;
 					return (
-						<Grid item sm={Number(size) * 12}>
+						<Grid item sm={Number(size) * 12} style={{ padding: "0 1em" }}>
 							<Input
 								key={`${this.id}-input-${i}`}
 								type={type}
