c75549866ae9b77ea890b16f085f77c92661c73b
zipang <christophe.Desguez@gmail.com>
2021-01-26 12:44:04 +0100

    Setting up ValidationContext prototype

--- src/components/forms/validator/ValidationContext.js
+++ src/components/forms/validator/ValidationContext.js
@@ -1,5 +1,13 @@
-function ValidationContext({ mode = "onBlur", defaultValues = {} }) {
+import { setProperty, getProperty } from "@lib/utils/NestedObjects";
+
+function ValidationContext({ defaultValues = {} }) {
 	this.fields = [];
 	this.errors = {};
 	this.data = { ...defaultValues }; // Clone the default values
 }
+
+ValidationContext.prototype = {
+	register: function (name, { inputRef, validation, serialize, format }) {
+		this.fields.push();
+	}
+};
