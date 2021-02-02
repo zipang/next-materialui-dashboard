e2116bbf80a416238a0ad79950f4cb046d73cf39
zipang <christophe.Desguez@gmail.com>
2021-02-02 15:30:05 +0100

    Working CheckBoxes to input multiple values

---
+++ src/components/forms/inputs/CheckBoxes.js
@@ -0,0 +1,133 @@
+import { createRef, useState, useEffect, useLayoutEffect } from "react";
+import { Checkbox as MaterialCheckbox, FormControlLabel, Grid } from "@material-ui/core";
+import GroupLabel from "@forms/GroupLabel";
+import { useFormValidationContext } from "@forms/validation/FormValidationProvider";
+
+const _BASE_INPUT_STYLES = {
+	variant: "outlined",
+	fontSize: "large",
+	margin: "dense"
+};
+
+/**
+ * Takes a map code:value and return an array
+ * @param {Object} map
+ */
+const convertOptions = (map) =>
+	Object.keys(map).reduce((options, key) => {
+		options.push({ code: key, label: map[key] });
+		return options;
+	}, []);
+
+/**
+ * @typedef CheckBoxesProps
+ * @field {String} name
+ * @field {String} label
+ * @field {Array|Object} options Available options to choose from (code + label) or map
+ */
+/**
+ * A group of checkboxes used to display an array of possible values
+ * @param {CheckBoxesProps} props
+ */
+const CheckBoxes = ({
+	name = "checkbox",
+	label = "",
+	options = [],
+	required = false,
+	autoFocus = false,
+	defaultValue = [],
+	validation = {},
+	width = 1 / 3, // checkboxes are arranged in 3 columns
+	...moreProps
+}) => {
+	const inputRef = createRef(); // This ref will reference the first checkbox in the serie
+
+	// Find the Form Validation Context to register our input
+	const { register, errors, setData, getData, validate } = useFormValidationContext();
+
+	// Register our Input so that the validation can take effect
+	register(name, { inputRef, required, defaultValue, validation });
+
+	// Get the current checked values from the ValidationContext
+	const [values, setValues] = useState(getData(name));
+
+	// This field value is in fact an array of values corresponding to checked elements
+	const mergedProps = { ..._BASE_INPUT_STYLES, ...moreProps };
+
+	// Accept a hashmap (key : value) as different format
+	if (!Array.isArray(options)) options = convertOptions(options);
+
+	/**
+	 * When a single checkbox change : add or remove the value from the list
+	 * @param {Event} evt
+	 */
+	const onChange = (evt) => {
+		const valueToToggle = evt.target.value;
+		if (values.includes(valueToToggle)) {
+			setData(
+				name,
+				values.filter((val) => val !== valueToToggle)
+			);
+		} else {
+			setData(name, [...values, valueToToggle]);
+		}
+		setValues(getData(name));
+		if (evt.key === "Enter" && !evt.shiftKey) {
+			validate();
+		}
+	};
+
+	useLayoutEffect(() => {
+		if (autoFocus) {
+			inputRef.current.focus();
+		}
+	}, [name]);
+
+	return (
+		<GroupLabel label={label}>
+			<Grid container>
+				{options.map(({ code, label }, i) => {
+					if (i === 0) {
+						return (
+							<Grid item sm={Number(width * 12)}>
+								<FormControlLabel
+									label={label}
+									key={`${name}:${code}-label`}
+									control={
+										<MaterialCheckbox
+											name={`${name}:${code}`}
+											key={`${name}:${code}`}
+											inputRef={inputRef} // the ref for focus is on the first element
+											value={code}
+											checked={values.includes(code)}
+											onChange={onChange}
+										/>
+									}
+								/>
+							</Grid>
+						);
+					} else {
+						return (
+							<Grid item sm={Number(width * 12)}>
+								<FormControlLabel
+									label={label}
+									control={
+										<MaterialCheckbox
+											name={`${name}:${code}`}
+											key={`${name}:${code}`}
+											value={code}
+											checked={values.includes(code)}
+											onChange={onChange}
+										/>
+									}
+								/>
+							</Grid>
+						);
+					}
+				})}
+			</Grid>
+		</GroupLabel>
+	);
+};
+
+export default CheckBoxes;
---
+++ src/components/forms/inputs/CheckBoxes.stories.js
@@ -0,0 +1,83 @@
+// CheckBoxes.stories.js
+import CheckBoxes from "./CheckBoxes";
+import { FormValidationProvider } from "@forms/validation/FormValidationProvider";
+import { VForm } from "@forms/validation/VForm";
+
+const musicOptions = {
+	rock: "Rock",
+	punk: "Punk",
+	indie: "Indie Rock",
+	garage: "Garage",
+	soul: "Soul",
+	funk: "Funk",
+	rnb: "Rythm & Blues",
+	blues: "Blues",
+	jazz: "Jazz",
+	easy: "Easy Listening",
+	kpop: "KPop",
+	jpop: "JPop"
+};
+const saucesOptions = [
+	{ code: "oyster", label: "Sauce Huitre" },
+	{ code: "gingember", label: "Sauce Gingembre" },
+	{ code: "garlic", label: "Sauce Ail" },
+	{ code: "ketchup", label: "Sauce Ketchup" },
+	{ code: "bbq", label: "Sauce BBQ" }
+];
+
+// This default export determines where your story goes in the story list
+export default {
+	title: "CheckBoxes Input with validation",
+	component: CheckBoxes,
+	args: {
+		required: false,
+		autoFocus: true,
+		readOnly: false
+	},
+	argTypes: {
+		required: { control: { type: "boolean" } },
+		autoFocus: { control: { type: "boolean" } },
+		readOnly: { control: { type: "boolean" } },
+		defaultValue: { control: { type: null } },
+		name: { control: { type: null } },
+		label: { control: { type: null } }
+	}
+};
+
+export const SimpleCheckBoxesOutput = ({ ...args }) => (
+	<FormValidationProvider>
+		<VForm id="simple-checkboxes-input">
+			<CheckBoxes
+				{...args}
+				name="music_choices"
+				label="Choose your music"
+				options={musicOptions}
+			/>
+		</VForm>
+	</FormValidationProvider>
+);
+
+export const MoreCheckBoxesInputs = ({ prefix, suffix, ...args }) => (
+	<FormValidationProvider data={{ music_choices: ["punk", "rock", "indie", "garage"] }}>
+		<VForm id="more-checkboxes-inputs">
+			<CheckBoxes
+				{...args}
+				name="music_choices"
+				label="Your music choices"
+				readOnly={true}
+				options={musicOptions}
+				helperText="Read only"
+			/>
+			<CheckBoxes
+				{...args}
+				name="alternate_choices"
+				label="Choose more than 2 sauces"
+				options={saucesOptions}
+				validation={{
+					tooFew: (choices) =>
+						choices.length < 2 ? "Not enough choices" : true
+				}}
+			/>
+		</VForm>
+	</FormValidationProvider>
+);
