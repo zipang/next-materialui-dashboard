e0c1dff15b5ab5b97b9809ac7b81f7d5f963e178
zipang <christophe.Desguez@gmail.com>
2021-02-04 18:26:06 +0100

    NEW Radio input (one choice amongst a few)

---
+++ src/components/forms/inputs/Radio.js
@@ -0,0 +1,115 @@
+import { createRef, useLayoutEffect } from "react";
+import {
+	RadioGroup,
+	Radio as MaterialRadio,
+	FormLabel,
+	FormControl,
+	FormHelperText,
+	FormControlLabel
+} from "@material-ui/core";
+import { useFormValidationContext } from "../validation/FormValidationProvider";
+import { convertOptions } from "@forms/validation/utils";
+import _BASE_INPUT_STYLES from "./styles";
+import { isUndefined } from "../validation/ValidationContext";
+
+/**
+ * @typedef RadioProps
+ * @param {String} name
+ * @param {String} label
+ * @field {Array|Object} options Available options to choose from (code + label) or map
+ */
+/**
+ * The Radio has several options to choose from.
+ * One selection is always required.
+ * The first option is always selected if the `defaultValue` is not passed
+ * @example
+ *   <Radio name="gender" label="Gender" options={{ M:"Male", F:"Female "}} />
+ * @param {RadioProps} props
+ */
+const Radio = ({
+	name = "radio",
+	label = "Select a value",
+	helperText = "",
+	options = [],
+	defaultValue,
+	autoFocus = false,
+	readOnly = false,
+	validation = {},
+	size = 1 / 3,
+	...moreProps
+}) => {
+	// Find the form validation context to register our input
+	const inputRef = createRef();
+	const { register, setData, getData, validate, errors } = useFormValidationContext();
+
+	// Accept a hashmap (key : value) as different format
+	if (!Array.isArray(options)) options = convertOptions(options);
+
+	// Select the first value by default
+	if (isUndefined(defaultValue)) defaultValue = options[0].code;
+
+	// Do not focus on read-only items
+	if (readOnly) autoFocus = false;
+
+	// Register our Input so that the validation can take effect
+	register(name, { inputRef, defaultValue, validation });
+
+	// Load the initial value from the Validation Context
+	const selectedValue = getData(name);
+	const errorMessage = errors[name]?.message || "";
+
+	const onChange = (evt) => {
+		const selectedValue = evt.target.value;
+		setData(name, selectedValue);
+		if (errorMessage) {
+			validate(name); // Show when the selection becomes valid again
+		}
+	};
+
+	useLayoutEffect(() => {
+		if (autoFocus) {
+			inputRef.current?.focus();
+		}
+	}, [name]);
+
+	return (
+		<FormControl
+			component="fieldset"
+			readOnly={readOnly}
+			error={Boolean(errorMessage)}
+		>
+			<FormLabel component="legend">{label}</FormLabel>
+			<RadioGroup
+				row
+				aria-label={label}
+				name={name}
+				// autoFocus={autoFocus}
+				defaultValue={selectedValue}
+				onChange={onChange}
+				{...moreProps}
+			>
+				{options.map(({ code, label }, i) =>
+					i === 0 ? (
+						<FormControlLabel
+							key={`${name}-${code}`}
+							inputRef={inputRef}
+							value={code}
+							control={<MaterialRadio />}
+							label={label}
+						/>
+					) : (
+						<FormControlLabel
+							key={`${name}-${code}`}
+							value={code}
+							control={<MaterialRadio />}
+							label={label}
+						/>
+					)
+				)}
+			</RadioGroup>
+			<FormHelperText>{errorMessage || helperText}</FormHelperText>
+		</FormControl>
+	);
+};
+
+export default Radio;
---
+++ src/components/forms/inputs/Radio.stories.js
@@ -0,0 +1,74 @@
+// Radio.stories.js
+import Radio from "./Radio";
+import { FormValidationProvider } from "@forms/validation/FormValidationProvider";
+import { VForm } from "@forms/validation/VForm";
+
+const musicOptions = {
+	rock: "Rock",
+	punk: "Punk",
+	indie: "Indie Rock"
+};
+const saucesOptions = [
+	{ code: "oyster", label: "Sauce Huitre" },
+	{ code: "gingember", label: "Sauce Gingembre" },
+	{ code: "garlic", label: "Sauce Ail" },
+	{ code: "ketchup", label: "Sauce Ketchup" },
+	{ code: "bbq", label: "Sauce BBQ" }
+];
+
+// This default export determines where your story goes in the story list
+export default {
+	title: "Radio Input with validation",
+	component: Radio,
+	args: {
+		required: false,
+		autoFocus: true,
+		readOnly: false
+	},
+	argTypes: {
+		required: { control: { type: "boolean" } },
+		autoFocus: { control: { type: "boolean" } },
+		readOnly: { control: { type: "boolean" } },
+		defaultValue: { control: { type: null } },
+		name: { control: { type: null } },
+		label: { control: { type: null } }
+	}
+};
+
+export const SimpleRadioOutput = ({ ...args }) => (
+	<FormValidationProvider>
+		<VForm id="simple-checkboxes-input">
+			<Radio
+				{...args}
+				name="choices.music"
+				label="What's the best music ?"
+				options={musicOptions}
+			/>
+		</VForm>
+	</FormValidationProvider>
+);
+
+export const MoreRadioInputs = ({ prefix, suffix, ...args }) => (
+	<FormValidationProvider data={{ choices: { music: "indie", sauce: "garlic" } }}>
+		<VForm id="more-checkboxes-inputs">
+			<Radio
+				{...args}
+				name="choices.music"
+				label="Your music choice"
+				readOnly={true}
+				options={musicOptions}
+				helperText="Read only"
+			/>
+			<Radio
+				{...args}
+				name="choices.sauce"
+				label="Your special sauce"
+				options={saucesOptions}
+				validation={{
+					notAvailable: (choice) =>
+						choice === "bbq" ? "This sauce is not available now" : true
+				}}
+			/>
+		</VForm>
+	</FormValidationProvider>
+);
