9fb844bff0a77de79f3918109b1571c954afaf96
zipang <christophe.Desguez@gmail.com>
2021-02-05 18:34:35 +0100

    Document the most basic example of a form with a validation context

--- src/components/forms/validation/VForm.js
+++ src/components/forms/validation/VForm.js
@@ -1,3 +1,4 @@
+import HiddenSubmit from "../inputs/HiddenSubmit";
 import useFormStyles from "../useFormStyles";
 import { useFormValidationContext } from "./FormValidationProvider";
 
@@ -5,22 +6,29 @@ import { useFormValidationContext } from "./FormValidationProvider";
  * @typedef VFormProps
  * @param {Function<data>} onSuccess Callback called when the form validation is a success. The validated data is provided.
  * @param {Function<errors>} onError Callback called when the form validation is a failure. The errors object is provided.
+ * @param {Boolean} validateOnEnter Add a hidden submit to catch the ENTER keypressed and trigger a form validation
  */
 
 /**
  * This form MUST be used inside a <FormValidationProvider>
  * @param {VFormProps} props
  */
-export const VForm = ({
+const VForm = ({
 	onSuccess = console.log,
 	onError = console.error,
+	validateOnEnter = false,
 	children,
 	...props
 }) => {
 	const styles = useFormStyles();
 	const { validate } = useFormValidationContext();
 
-	// Just log what's goin on when submitting
+	/**
+	 * Prevent for submission, validate the field values
+	 * And then call the appropriate callback
+	 * @param {DOMEvent}
+	 */
+
 	const onSubmit = (evt) => {
 		evt.preventDefault();
 		validate({
@@ -32,7 +40,9 @@ export const VForm = ({
 	return (
 		<form onSubmit={onSubmit} className={styles.form} {...props}>
 			{children}
-			<input type="submit" className="hidden" aria-hidden="true" />
+			{validateOnEnter && <HiddenSubmit />}
 		</form>
 	);
 };
+
+export default VForm;
