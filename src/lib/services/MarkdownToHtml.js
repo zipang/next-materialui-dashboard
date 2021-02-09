defdc8d8492e1b569978681e6db8f8caa576903a
zipang <christophe.Desguez@gmail.com>
2021-02-10 00:17:51 +0100

    Added new MarkdownToHtml service

---
+++ src/lib/services/MarkdownToHtml.js
@@ -0,0 +1,35 @@
+import micromark from "micromark";
+import combine from "micromark/dist/util/combine-extensions.js";
+import combineHtml from "micromark/dist/util/combine-html-extensions.js";
+import gfmTables from "micromark-extension-gfm-table";
+import gfmTablesHtml from "micromark-extension-gfm-table/html.js";
+import gfmAutoLinks from "micromark-extension-gfm-autolink-literal";
+import gfmAutoLinksHtml from "micromark-extension-gfm-autolink-literal/html.js";
+
+const _DEFAULT_OPTIONS = {
+	allowDangerousHtml: true,
+	extensions: combine([gfmTables, gfmAutoLinks]),
+	htmlExtensions: combineHtml([gfmTablesHtml, gfmAutoLinksHtml])
+};
+
+// const serializer = remark().use(breaks).use(gfm).use(links).use(html);
+
+/**
+ * Convert GFM markdown text to HTML
+ * @see https://github.github.com/gfm/
+ * @param {String} md Markdown text
+ * @param {Object}
+ * @return {String} html converted version
+ */
+export const convertToHtml = (md, options) => {
+	const startTime = Date.now();
+	try {
+		const html = micromark(md, { ..._DEFAULT_OPTIONS, ...options });
+		const elapsed = Date.now() - startTime;
+		console.log(`HTML conversion took ${elapsed}ms`);
+		return html;
+	} catch (err) {
+		console.error(err);
+		return `Conversion to HTML Failed : <code>${err.message}</code>`;
+	}
+};
---
+++ src/lib/services/MarkdownToHtml.spec.js
@@ -0,0 +1,40 @@
+import { suite } from "uvu";
+import code from "@hapi/code";
+import { convertToHtml } from "./MarkdownToHtml.js";
+
+const { expect } = code;
+
+const MarkdownToHtmlTestSuite = suite("Markdown to HTML conversion");
+
+const markdownText = `
+# Hello Markdown GFM
+
+Convert *Markdown GFM* syntax to HTML.
+Line-breaks are generated without the need of two spaces (\`'  '\`) at the end of each lines.
+
+Links like this one >> https://github.github.com/gfm/ << are automatically converted to hyperlinks.
+
+Cat images can be inserted.
+![Niam](https://media3.giphy.com/media/22kxQ12cxyEww/source.gif)
+
+## References
+
+* [The original Markdown post by John Gruber](https://daringfireball.net/projects/markdown/)
+
+`;
+
+MarkdownToHtmlTestSuite("Conversion", async () => {
+	const html = await convertToHtml(markdownText);
+
+	console.log("Converted Markdown text", html);
+
+	expect(html).to.contain([
+		`<h1>Hello Markdown GFM</h1>`,
+		"<h2>References</h2>",
+		`href="https://github.github.com/gfm/"`,
+		`alt="Niam"`
+	]);
+	expect(html).to.not.contain("<h3>");
+});
+
+MarkdownToHtmlTestSuite.run();
