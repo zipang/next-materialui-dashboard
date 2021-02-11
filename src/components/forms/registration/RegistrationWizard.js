309145bdab16d14f06d54151b0361e747a8cb7ec
CMLINVIE <78873486+CMLINVIE@users.noreply.github.com>
2021-02-11 09:29:41 +0100

    Update RegistrationWizard.js
    Ajout background nouvel organisme

--- src/components/forms/registration/RegistrationWizard.js
+++ src/components/forms/registration/RegistrationWizard.js
@@ -24,7 +24,7 @@ Ce process est un peu long mais vous pourrez l'interrompre et le reprendre à to
 		title: "Nouvel Organisme",
 		help: {
 			description: `Vérifiez l'adresse, le nom et la date de création de la structure à déclarer.`,
-				backgroundImage: "https://invie78.fr/images/background-registration.jpg"
+				backgroundImage: "https://invie78.fr/images/background-nouvel-organisme.jpg"
 		},
 		fields: [
 			{
