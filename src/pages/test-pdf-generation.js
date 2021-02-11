9e62fa4231ab5df6a0883d2addf99c073ad3371f
zipang <christophe.Desguez@gmail.com>
2021-02-12 00:23:41 +0100

    Test inline vs API responses

--- src/pages/test-pdf-generation.js
+++ src/pages/test-pdf-generation.js
@@ -2,26 +2,37 @@ import { createRef } from "react";
 import CenteredPaperSheet from "@components/CenteredPaperSheet";
 import Submit from "@components/forms/inputs/Submit";
 import useFormStyles from "@components/forms/useFormStyles";
-import PdfClient from "@lib/client/PdfClient";
+import { generateFromHtml } from "@lib/client/PdfApiClient";
 
 const TestGenerationPdfPage = () => {
-	const ref = createRef();
+	const refHtml = createRef();
+	const refInline = createRef();
 	const styles = useFormStyles({
-		minWidth: "1024px"
+		minWidth: "1024px",
+		"& label": {
+			display: "block",
+			width: "80%"
+		}
 	});
 	/**
 	 *
 	 * @param {DOMEvent} evt
 	 */
 	const sendHtmlToConvert = async (evt) => {
-		evt.preventDefault();
 		try {
-			const html = ref.current.value;
-			const { filename, content } = await PdfClient.generateFromHtml(
-				"test.pdf",
-				html
-			);
-			console.log(filename, content);
+			const html = refHtml.current.value;
+			const inline = refInline.current.checked;
+			if (inline) {
+				return; // We will be directed to the API response (a PDF document inline)
+			} else {
+				evt.preventDefault();
+				const { filename, content } = await generateFromHtml(
+					"test.pdf",
+					html,
+					false
+				);
+				console.log(filename, content);
+			}
 		} catch (err) {
 			console.error(err);
 		}
@@ -38,7 +49,12 @@ const TestGenerationPdfPage = () => {
 				<label>
 					Copy your HTML in the textarea
 					<br />
-					<textarea ref={ref} name="html" cols="80" rows="15"></textarea>
+					<textarea ref={refHtml} name="html" cols="120" rows="15"></textarea>
+				</label>
+				<label>
+					See the generated PDF in the browser
+					<br />
+					<input ref={refInline} name="inline" type="checkbox" />
 				</label>
 				<Submit label="Send" />
 			</form>
