0e18d80af3ae72932e3c848df9ff241ba55170dc
zipang <christophe.Desguez@gmail.com>
2021-02-12 11:29:08 +0100

    Webpack template loader and generation

--- src/scripts/generateTemplate.js
+++ src/scripts/generateTemplate.js
@@ -88,7 +88,13 @@ const ${templateName} = {
 	html${propertiesKeys}
 };
 
-export default ${templateName};
+const render = (data) =>
+	Object.keys(${templateName}).reduce((prev, key) => {
+		prev[key] = ${templateName}[key](data);
+		return prev;
+	}, {});
+
+export default render;
 `;
 	return templateSource;
 };
@@ -99,14 +105,18 @@ export default MarkdownTemplateLoader;
  * Command line
  */
 const generation = async () => {
+	// Chhose the markdown template
 	terminal.cyan("Choose a Markdown template file (use TAB for completion): ");
-	const filename = await terminal.fileInput({
-		baseDir: "../templates",
+	const markdownFile = await terminal.fileInput({
+		baseDir: "../../content/templates",
 		autoComplete: true
 	});
-	const templateName = path.basename(filename, ".md");
+	// Extract its name
+	const templateName = path.basename(markdownFile, ".md");
+	// Load its content and front-ammer with gray-matter
+	const { data, content } = gm.read(markdownFile);
 
-	const { data, content } = gm.read(filename);
+	// Report
 	terminal.blue(`
 Front matter data :
 	${JSON.stringify(data)}
@@ -114,8 +124,19 @@ Front matter data :
 	terminal.green(`
 Markdown content :
 ${content}`);
+
+	// Generate the compiled js
 	const fileSource = MarkdownTemplateLoader(templateName, content, data);
-	const destination = filename.replace(/.md$/, ".js");
+
+	// The generated JS will be located inside src/templates
+	const destination = path.join(
+		path.dirname(markdownFile),
+		"../../../src/templates",
+		path.basename(markdownFile).replace(/.md$/, ".js")
+	);
+	terminal.cyan(`Copying the JS template to :
+${destination}
+`);
 
 	await fs.writeFile(destination, fileSource);
 	terminal.green(`Template ${templateName}.js has been generated !
