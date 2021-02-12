c63801374429288dc3d97eac710981b95f03d2dd
zipang <christophe.Desguez@gmail.com>
2021-02-12 15:49:06 +0100

    Features testing

---
+++ src/pages/tests/email-template.js
@@ -0,0 +1,62 @@
+import CenteredPaperSheet from "@components/CenteredPaperSheet";
+import useFormStyles from "@components/forms/useFormStyles";
+import { sendMailTemplate } from "@lib/client/MailApiClient";
+import APIForm from "@components/forms/APIForm";
+import { SiretInput } from "@components/forms/registration/SiretSearch";
+import Text from "@components/forms/inputs/Text";
+import Email from "@components/forms/inputs/Email";
+import Submit from "@components/forms/inputs/Submit";
+
+const TestEmailTemplatePage = () => {
+	const styles = useFormStyles({
+		minWidth: "1024px",
+		"& label": {
+			display: "block",
+			width: "80%"
+		}
+	});
+
+	/**
+	 *
+	 * @param {DOMEvent} evt
+	 */
+	const sendMessageTemplate = async (formData) => {
+		try {
+			formData.date_creation = new Date().toISOString().substr(0, 10);
+			const resp = await sendMailTemplate("welcome", formData);
+			alert(JSON.stringify(resp, null, "\t"));
+		} catch (err) {
+			console.error(err);
+		}
+	};
+	return (
+		<CenteredPaperSheet xs={10} md={8}>
+			<APIForm className={styles.form} onSubmit={sendMessageTemplate}>
+				<h1>Send a welcome email to</h1>
+				<SiretInput name="siret" />
+				<Text
+					name="nom"
+					label="Nom structure"
+					required="Saissiez un nom d'Organisme"
+				/>
+				<Text
+					name="representant.prenom"
+					label="Prénom"
+					required="Saisissez le prénom du représentant"
+				/>
+				<Text
+					name="representant.nom"
+					label="Nom"
+					required="Saisissez le nom du représentant"
+				/>
+				<Email
+					name="representant.email"
+					label="Email"
+					required="Saisissez l'email' du représentant"
+				/>
+				<Submit label="Send" />
+			</APIForm>
+		</CenteredPaperSheet>
+	);
+};
+export default TestEmailTemplatePage;
