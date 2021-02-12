9f2f341a326d5010897405269971eb6eda18dc42
zipang <christophe.Desguez@gmail.com>
2021-02-12 11:32:17 +0100

    Rely on Mailer to recreate the attachments

--- src/pages/api/mail/send.js
+++ src/pages/api/mail/send.js
@@ -18,19 +18,11 @@ import Mailer from "@lib/services/Mailer";
 export default async (req, resp) => {
 	const message = req.body;
 
-	// Recreate the Buffers in the attachments
-	if (Array.isArray(message.attachments)) {
-		message.attachments = message.attachments.map((a) => {
-			a.content = Buffer.from(a.content);
-			return a;
-		});
-	}
-
 	try {
 		const mailerResponse = await Mailer.sendMail(message);
 		resp.json({
 			success: true,
-			message: `Mail '${subject}' sent`,
+			message: `Mail '${message.subject}' has been sent`,
 			...mailerResponse
 		});
 	} catch (err) {
