02c6d7c576290779543c43f9fe045ea81a93282d
zipang <christophe.Desguez@gmail.com>
2021-02-12 11:36:29 +0100

    API to render posted data through templates

---
+++ src/pages/api/templates/[name].js
@@ -0,0 +1,26 @@
+import { render } from "@lib/services/Templates";
+
+/**
+ * The API entry point (POST) to render a given template
+ * Pass the data into the JSON body
+ */
+export default async (req, resp) => {
+	try {
+		const { name } = req.query; // Name of the template file to use
+		const data = req.body;
+		console.log(`Received ${data} to render with ${name} template`);
+		loadEnv(data);
+
+		const content = await render(name, data);
+
+		resp.json({
+			success: true,
+			content
+		});
+	} catch (err) {
+		resp.status(err.code).json({
+			success: false,
+			error: err.message
+		});
+	}
+};
