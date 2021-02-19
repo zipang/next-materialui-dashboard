cea35b8799baad2d02b2fb8784579fc000ba6d44
zipang <christophe.Desguez@gmail.com>
2021-02-19 22:59:57 +0100

    DRY refactoring : Much better error handling

--- src/lib/client/ApiClient.js
+++ src/lib/client/ApiClient.js
@@ -3,56 +3,97 @@ import ApiError from "../ApiError.js";
 
 const APIClient = {};
 
-/**
- * We expect the response to be in JSON format, but sometimes.. shit happens..
- * @param {HttpResponse} resp
- * @return {Object}
- */
-const readResponseBody = async (resp) => {
-	try {
-		return await resp.json();
-	} catch (err) {
-		// Error parsing JSON format
-		console.log(resp);
-		// const textBody = await resp.text();
-		return {
-			code: resp.status,
-			success: false,
-			error: resp.statusText
-		};
-	}
+const _DEFAULT_REQUEST_HEADERS = {
+	Accept: "application/json",
+	"Content-Type": "application/json"
 };
 
-export const get = (APIClient.get = async (...apiParams) => {});
-
 /**
- * POST to Next.js API
- * @param {String} postUrl
- * @param {Object} postBody
- * @return {Promise<Object>} return the parsed API response
+ * Complete relative API URLs with the base site URL
+ * that must exist as an environment variable
+ * @param {String} apiEndpoint
+ * @return {URL}
  */
-export const post = (APIClient.post = async (postUrl, postBody = {}) => {
-	// Complete the relative URL
-	if (!postUrl.startsWith("http")) {
+export const buildURL = (apiEndpoint, params) => {
+	let finalUrl;
+	if (apiEndpoint.startsWith("http")) {
+		finalUrl = new URL(apiEndpoint);
+	} else {
+		// Complete the relative URL
 		const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
 		if (!baseUrl || !baseUrl.startsWith("http")) {
 			throw new ApiError(
 				500,
-				`ApiClient.post() failed : the environment variable NEXT_PUBLIC_SITE_URL is ${baseUrl}`
+				`ApiClient call failed : the environment variable NEXT_PUBLIC_SITE_URL is ${baseUrl}`
 			);
 		}
-		postUrl = new URL(postUrl, baseUrl);
+		finalUrl = new URL(apiEndpoint, baseUrl.replace("6006", "3000")); // Replace Storybook port number by dev port number to allow testing from Storybook
 	}
+	// Append the query parameters
+	if (typeof params === "object") {
+		Object.keys(params).forEach((paramName) =>
+			finalUrl.searchParams.append(paramName, params[paramName])
+		);
+	}
+	console.log(`buildUrl() : ${finalUrl.toString()}`);
+	return finalUrl;
+};
 
-	const resp = await fetch(postUrl, {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify(postBody)
+/**
+ *
+ * @param {ReadableStream} stream
+ * @return {Promise<String>}
+ */
+const readStreamAsText = async (stream) => {
+	const chunks = [];
+	return new Promise((resolve, reject) => {
+		stream.on("data", (chunk) => chunks.push(chunk));
+		stream.on("error", reject);
+		stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
 	});
+};
 
+/**
+ * We expect the response to be in JSON format, but sometimes.. shit happens..
+ * @see https://stackoverflow.com/questions/40497859/reread-a-response-body-from-javascripts-fetch
+ * For an explanation of the strategy of cloning the response before trying to parse it to JSON
+ * @param {HttpResponse} resp
+ * @return {Object}
+ */
+const readResponseBody = async (resp) => {
+	let retry;
 	try {
+		// retry = resp.clone(); // Don't bother
+		return await resp.json();
+	} catch (err) {
+		// Error parsing JSON format
+		// The stream is locked and will return a TypeError "Response.arrayBuffer: Body has already been consumed"
+		// if we re-try to read it
+		// We must read the cloned response instead
+		// const respText = await retry.text();
+		return {
+			code: resp.status,
+			success: false,
+			error: retry.statusText // statusText is usually enough to understand what's going on (404)
+		};
+	}
+};
+
+/**
+ * HTTP GET on our Next.js local API
+ * @param {String} apiEntryPoint
+ * @param {Object} [apiParams] Optional parameters (as key-value pairs)
+ * @return {Object} the parsed JSON response if no error was thrown
+ * @throws {ApiError}
+ */
+export const get = (APIClient.get = async (apiEntryPoint, apiParams) => {
+	let resp;
+	try {
+		resp = await fetch(buildURL(apiEntryPoint, apiParams), {
+			method: "GET",
+			headers: _DEFAULT_REQUEST_HEADERS
+		});
 		const respBody = await readResponseBody(resp);
-		console.dir(`API POST to ${postUrl} returned`, respBody);
 
 		if (respBody.error) {
 			throw new ApiError(respBody.code, respBody.error);
@@ -60,8 +101,46 @@ export const post = (APIClient.post = async (postUrl, postBody = {}) => {
 
 		return respBody;
 	} catch (err) {
-		console.error(`POST to ${postUrl} raised an API error response`, err);
-		throw new ApiError(resp.status, `${err.message} (${resp.statusText})`);
+		console.error(
+			`GET to ${apiEntryPoint} raised an API error response`,
+			err.message
+		);
+		const errorCode = err.code || resp?.status || 500;
+		throw new ApiError(
+			errorCode,
+			`Call to ${apiEntryPoint} failed : ${err.message} (${errorCode})`
+		);
+	}
+});
+
+/**
+ * POST to Next.js API
+ * @param {String} postUrl
+ * @param {Object} postBody Body data
+ * @return {Promise<Object>} return the parsed API response
+ */
+export const post = (APIClient.post = async (apiEntryPoint, postBody = {}) => {
+	try {
+		const resp = await fetch(buildURL(apiEntryPoint), {
+			method: "POST",
+			headers: _DEFAULT_REQUEST_HEADERS,
+			body: JSON.stringify(postBody)
+		});
+
+		const respBody = await readResponseBody(resp);
+		console.dir(`API POST to ${apiEntryPoint} returned`, respBody);
+
+		if (respBody.error) {
+			throw new ApiError(respBody.code, respBody.error);
+		}
+
+		return respBody;
+	} catch (err) {
+		console.error(`POST to ${apiEntryPoint} raised an API error response`, err);
+		throw new ApiError(
+			err.code || 500,
+			`Call to ${apiEntryPoint} failed : ${err.message}`
+		);
 	}
 });
 
