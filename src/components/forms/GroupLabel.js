7e7bb62eb281e5ddef94d70c746a84cc24fa5bbe
cdesguez <christophe.desguez@gmail.com>
2021-03-07 05:16:23 +0100

    Put the label outside or inside

--- src/components/forms/GroupLabel.js
+++ src/components/forms/GroupLabel.js
@@ -1,26 +1,47 @@
 import { Box } from "@material-ui/core";
 
 /**
+ * @typedef GroupLabelProps
+ * @field {String} label
+ * @field {Boolean} [labelInside=true] Put the label inside the box
+ */
+
+/**
  * Label a group of controls
  * and draw a box around them
- * @param {PropsWithChildren} props
- * @param {String} props.label
+ * @param {GroupLabelProps} props
  */
-const GroupLabel = ({ label = "", children }) => (
-	<Box
-		marginTop="0.5em"
-		marginBottom="0.5em"
-		display="flex"
-		flexWrap="wrap"
-		flexGrow="1"
-		padding="0.5em"
-		border="1px solid #ccc"
-	>
-		<Box component="label" display="block" width="100%" margin="0.1em 0 0.1em 0.5em">
-			{label}
+const GroupLabel = ({ label = "", labelInside = true, children }) => (
+	<>
+		{!labelInside && (
+			<Box component="label" display="block" width="100%" margin="1em 0 0 0.5em">
+				<h3 style={{ marginBottom: 0 }}>
+					<strong>{label}</strong>
+				</h3>
+			</Box>
+		)}
+		<Box
+			marginTop="0.5em"
+			marginBottom="0.5em"
+			display="flex"
+			flexWrap="wrap"
+			flexGrow="1"
+			padding="0.5em"
+			border="1px solid #ccc"
+		>
+			{labelInside && (
+				<Box
+					component="label"
+					display="block"
+					width="100%"
+					margin="0.1em 0 0.1em 0.5em"
+				>
+					{label}
+				</Box>
+			)}
+			{children}
 		</Box>
-		{children}
-	</Box>
+	</>
 );
 
 export default GroupLabel;
