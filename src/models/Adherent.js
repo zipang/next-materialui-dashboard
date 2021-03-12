5b379090a812484e7a9ec48d6e29a3bcfcc728ef
zipang <christophe.Desguez@gmail.com>
2021-03-12 21:01:48 +0100

    Export individual methods

--- src/models/Adherent.js
+++ src/models/Adherent.js
@@ -2,83 +2,71 @@ import Parse from "parse/node.js";
 import { getParseInstance } from "./ParseSDK.js";
 import ApiError from "../lib/ApiError.js";
 
+const LIST_OPTIONS = {
+	json: true,
+	select: [""]
+};
+
 /**
  * This is the server only model to manipulate Adherent
  */
-class _Adherent extends Parse.Object {
+export class Adherent extends Parse.Object {
 	constructor(data) {
 		// Just copy all the attributes
 		super("Adherent", data);
 		// return ParseProxy(this); // Will proxy to the get and set methods for all not found properties
 	}
-
-	/**
-	 * @return {Parse.User} The User that has created this adherent
-	 */
-	getOwner() {
-		this.fetch();
-		return this.owner;
-	}
 }
 
-Parse.Object.registerSubclass("Adherent", _Adherent);
+// Parse.Object.registerSubclass("Adherent", _Adherent);
 
-const StaticMethods = {
-	/**
-	 * Register a new Adherent
-	 * @param {Parse.User} owner
-	 * @param {Object} orgData
-	 * @return Adherent
-	 */
-	register: async (owner, orgData) => {
-		try {
-			const org = new _Adherent(orgData);
-			org.set("owner", owner);
-			delete org.env; // Silly
-			return await org.save(null, { cascadeSave: false });
-		} catch (err) {
-			console.error("Adherent.register()", err);
-			if (err.code === 137) {
-				throw new ApiError(
-					409,
-					`Adherent with siret '${orgData.siret}' already exist`
-				);
-			} else {
-				throw new ApiError(
-					err.code || 500,
-					`Registration of adherent '${orgData.nom}' failed : ${err.message}`
-				);
-			}
+export const update = async (data) => {
+	try {
+		const { siret } = data;
+		let adherent = await retrieveBySiret(siret);
+
+		if (!adherent) {
+			// Start with a creation
+			adherent = new Adherent(data);
+		} else {
+			// Update every property
+			adherent.set(data);
+			// Object.keys(data).forEach((key) => {
+			// 	adherent.set(key, data[key]);
+			// });
 		}
-	},
-
-	/**
-	 * Retrieves an existing Adherent by its siret number
-	 * @param {String} siret
-	 * @return {Adherent}
-	 */
-	retrieveBySiret: async (siret) => {
-		const Parse = getParseInstance(); // Because this instance has been augmented with new utility methods
-		return Parse.retrieveByUniqueKey("Adherent", "siret", siret);
-	},
-
-	/**
-	 * Retrieve a list of Adherent that match some filter by example criterias
-	 * @param {Object} params
-	 */
-	retrieve: async (params = {}) => {
-		try {
-			const Parse = getParseInstance();
-			const query = new Parse.Query("Adherent");
-			return query.find();
-		} catch (err) {
-			console.error(err);
-			throw new ApiError(
-				err.code || 500,
-				`Failed loading adherents : ${err.message}`
-			);
-		}
+		return await adherent.save();
+	} catch (err) {
+		console.error("Adherent.update()", err);
+		throw new ApiError(
+			err.code || 500,
+			`Update of adherent '${data.nom}' failed : ${err.message}`
+		);
 	}
 };
 
-export const Adherent = Object.assign(_Adherent, StaticMethods);
+/**
+ * Retrieves an existing Adherent by its siret number
+ * @param {String} siret
+ * @return {Adherent}
+ */
+export const retrieveBySiret = async (siret) => {
+	const Parse = getParseInstance(); // Because this instance has been augmented with new utility methods
+	return Parse.retrieveByUniqueKey("Adherent", "siret", siret);
+};
+
+/**
+ * Retrieve a list of Adherent that match some filter by example criterias
+ * @param {Object} params
+ */
+export const retrieve = async (params = {}) => {
+	try {
+		const Parse = getParseInstance();
+		const query = new Parse.Query("Adherent");
+		Parse.addQueryParameters(query, params);
+		return query.findAll();
+	} catch (err) {
+		console.error(err);
+		throw new ApiError(err.code || 500, `Failed loading adherents : ${err.message}`);
+	}
+};
--- src/models/Adhesion.js
+++ src/models/Adhesion.js
@@ -1,7 +1,19 @@
 import Parse from "parse/node.js";
 import { getParseInstance } from "./ParseSDK.js";
 import { getNextAdhesionNumber, updateAdhesionNumber } from "./Parameters.js";
+import { retrieveBySiret } from "./Adherent.js";
 import ApiError from "../lib/ApiError.js";
+
+// Extract these fields only when requesting a list
+const _DEFAULT_FIELD_LIST = [
+	"no",
+	"siret",
+	"nom",
+	"mode_paiement",
+	"date_debut",
+	"date_fin",
+	"statut"
+];
 
 /**
  * This is the server only model to manipulate Adhesion
@@ -11,14 +23,6 @@ class _Adhesion extends Parse.Object {
 		// Just copy all the attributes
 		super("Adhesion", data);
 		// return ParseProxy(this); // Will proxy to the get and set methods for all not found properties
-	}
-
-	/**
-	 * @return {Parse.Adherent} The Adherent
-	 */
-	getAdherent() {
-		this.fetch();
-		return this.adherent;
 	}
 }
 
@@ -33,29 +37,28 @@ Parse.Object.registerSubclass("Adhesion", _Adhesion);
  * @return Adhesion
  */
 export const create = async (siret, data = {}) => {
+	let no;
 	try {
-		const Parse = getParseInstance();
-		const adherent = await Parse.Adherent.retrieveBySiret(siret);
+		const adherent = await retrieveBySiret(siret);
 		// Get the next unique number
-		const no = await getNextAdhesionNumber();
-		const adh = new _Adhesion({
+		no = await getNextAdhesionNumber();
+		const adhesion = new _Adhesion({
 			no,
 			siret,
 			statut: "en_attente",
 			nom: adherent.get("nom"), // We need that duplicate information to avoid fetching the whole adherent
 			...data
 		});
-		adh.set("adherent", adherent);
-		await Promise.allSettled([
-			adh.save(null, { cascadeSave: false }),
-			updateAdhesionNumber(no)
-		]);
-		return adh;
+		adhesion.set("adherent", adherent);
+		await adhesion.save(null, { cascadeSave: false });
+		// Ok we can update the next adhesion no
+		updateAdhesionNumber(no);
+		return adhesion;
 	} catch (err) {
 		console.error(err);
 		throw new ApiError(
 			err.code || 500,
-			`Creation of new adhesion for '${siret}' failed : ${err.message}`
+			`Creation of new adhesion ${no} for '${siret}' failed : ${err.message}`
 		);
 	}
 };
@@ -63,13 +66,16 @@ export const create = async (siret, data = {}) => {
 /**
  * Retrieve a list of Adhesion that match some filter by example criterias
  * @param {Object} params
+ * @param {Array<String>} fields An array of field names to include
+ * @return {Array<Object>} These will not be Parse.Object
  */
-export const retrieve = async (params = {}) => {
+export const retrieve = async (params = {}, fields = _DEFAULT_FIELD_LIST) => {
 	try {
 		const Parse = getParseInstance();
 		const query = new Parse.Query("Adhesion");
-		const adhesions = await query.find();
-		console.log(`Retrieving ${adhesions.length} adhesions`);
+		query.select(fields);
+		Parse.addQueryParameters(query, params);
+		const adhesions = await query.findAll();
 		return adhesions.map((adh) => adh.toJSON());
 	} catch (err) {
 		console.error(err);
@@ -90,6 +96,7 @@ export const retrieveByNo = async (no) => {
 /**
  * Notify that the payment for this adhesion has been received.
  * Set the status of the adhesion and adherent to active
+ * Set the dates
  * @param {String} no
  * @param {Object} data
  */
@@ -101,12 +108,22 @@ export const confirmPayment = async (no, data = {}) => {
 		const date_debut = adhesion.get("date_debut");
 		if (!date_debut || currentDate > date_debut) {
 			adhesion.set("date_debut", currentDate);
+			const [year, month, day] = currentDate.split("-");
+			adhesion.set(
+				"date_fin",
+				[
+					(Number(year) + 1).toString(),
+					month,
+					day === "28" && month === "02" ? "27" : day
+				].join("-")
+			);
 		}
 		adhesion.set("statut", "active");
 		adhesion.set("paiement", data);
 
 		const adherent = adhesion.get("adherent");
 		adherent.set("statut", "actif");
+
 		await Promise.allSettled([adherent.save(), adhesion.save()]);
 		return adhesion;
 	} catch (err) {
@@ -117,10 +134,3 @@ export const confirmPayment = async (no, data = {}) => {
 		);
 	}
 };
-
-export const Adhesion = Object.assign(_Adhesion, {
-	create,
-	confirmPayment,
-	retrieveByNo,
-	retrieve
-});
