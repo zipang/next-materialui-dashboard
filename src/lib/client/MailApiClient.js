4a014168a49847b659688ad686527c3a1acb6dfd
zipang <christophe.Desguez@gmail.com>
2021-03-15 20:38:05 +0100

    Make it work on client and server

--- src/lib/client/MailApiClient.js
+++ src/lib/client/MailApiClient.js
@@ -1,16 +1,15 @@
 import APIClient from "./ApiClient";
 import ApiError from "@lib/ApiError";
-import { render } from "@lib/services/Templates";
 
 /**
- * Use a named template to build a message and send it
+ * Use a named template to build a message and send it by mail
  * @param {String} name
  * @param {Object} data
  */
 export const sendMailTemplate = async (name, data) => {
 	try {
-		const message = await render(name, data);
-		return await sendMessage(message);
+		const { content } = await renderTemplate(name, data);
+		return await sendMessage(content);
 	} catch (err) {
 		throw new ApiError(err.code || 500, err.message);
 	}
@@ -29,7 +28,22 @@ export const sendMessage = async (message) => {
 	}
 };
 
+/**
+ * Send the fully constructed MailMessage through the local API
+ * @param {String} name Template name
+ * @param {Object} data Data to render
+ * @return {Object}
+ */
+export const renderTemplate = async (name, data) => {
+	try {
+		return await APIClient.post(`/api/template/${name}`, data);
+	} catch (err) {
+		throw new ApiError(err.code || 500, err.message);
+	}
+};
+
 const MailApiClient = {
+	renderTemplate,
 	sendMailTemplate,
 	sendMessage
 };
