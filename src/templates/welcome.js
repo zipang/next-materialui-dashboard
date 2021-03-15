8c7fa8605fb18e4617ff4ebcbc19140abbbf096b
zipang <christophe.Desguez@gmail.com>
2021-03-15 20:39:33 +0100

    prettier

--- src/templates/welcome.js
+++ src/templates/welcome.js
@@ -1,5 +1,4 @@
-
-// welcome.js	
+// welcome.js
 const splitPath = (path = "") => path.split(/[,\[\]\.]+?/).filter(Boolean);
 
 /**
@@ -26,9 +25,20 @@ const getProperty = (source = {}, path = "", defaultValue) => {
  * @param {Object} data
  * @return {String}
  */
-export const text = function anonymous(data
-) {
-var out='<img src="https://invie78.fr/images/logo.jpg" alt="logo" />\n\nParis, le '+((new Date).toISOString().substr(0,10))+'\n\nBonjour **'+ (getProperty(data, "representant.prenom", ""))+' '+ (getProperty(data, "representant.nom", ""))+'**,\n\nL\'enregistrement de votre société **'+ (getProperty(data, "nom", ""))+'** a bien été pris en compte en date du '+ (getProperty(data, "date_creation", ""))+'.\n\n';return out.replace(/(<([^>]+)>)/gi, "");
+export const text = function anonymous(data) {
+	var out =
+		'<img src="https://invie78.fr/images/logo.jpg" alt="logo" />\n\nParis, le ' +
+		new Date().toISOString().substr(0, 10) +
+		"\n\nBonjour **" +
+		getProperty(data, "representant.prenom", "") +
+		" " +
+		getProperty(data, "representant.nom", "") +
+		"**,\n\nL'enregistrement de votre société **" +
+		getProperty(data, "nom", "") +
+		"** a bien été pris en compte en date du " +
+		getProperty(data, "date_creation", "") +
+		".\n\n";
+	return out.replace(/(<([^>]+)>)/gi, "");
 };
 
 /**
@@ -36,60 +46,73 @@ var out='<img src="https://invie78.fr/images/logo.jpg" alt="logo" />\n\nParis, l
  * @param {Object} data
  * @return {String}
  */
-export const html = function anonymous(data
-) {
-var out='<img src="https://invie78.fr/images/logo.jpg" alt="logo" /><p>Paris, le '+((new Date).toISOString().substr(0,10))+'</p><p>Bonjour <strong>'+ (getProperty(data, "representant.prenom", ""))+' '+ (getProperty(data, "representant.nom", ""))+'</strong>,</p><p>L\'enregistrement de votre société <strong>'+ (getProperty(data, "nom", ""))+'</strong> a bien été pris en compte en date du '+ (getProperty(data, "date_creation", ""))+'.</p>';return out;
+export const html = function anonymous(data) {
+	var out =
+		'<img src="https://invie78.fr/images/logo.jpg" alt="logo" /><p>Paris, le ' +
+		new Date().toISOString().substr(0, 10) +
+		"</p><p>Bonjour <strong>" +
+		getProperty(data, "representant.prenom", "") +
+		" " +
+		getProperty(data, "representant.nom", "") +
+		"</strong>,</p><p>L'enregistrement de votre société <strong>" +
+		getProperty(data, "nom", "") +
+		"</strong> a bien été pris en compte en date du " +
+		getProperty(data, "date_creation", "") +
+		".</p>";
+	return out;
 };
-
 
 /**
  * Front matter subject
  * @param {Object} data
  * @return {String}
  */
-export const subject = function anonymous(data
-) {
-var out='L\'enregistrement de '+ (getProperty(data, "nom", ""))+' a bien été reçu';return out;
-}
-
+export const subject = function anonymous(data) {
+	var out = "L'enregistrement de " + getProperty(data, "nom", "") + " a bien été reçu";
+	return out;
+};
 
 /**
  * Front matter to
  * @param {Object} data
  * @return {String}
  */
-export const to = function anonymous(data
-) {
-var out='"'+ (getProperty(data, "representant.prenom", ""))+' '+ (getProperty(data, "representant.nom", ""))+'" <'+ (getProperty(data, "representant.email", ""))+'>';return out;
-}
-
+export const to = function anonymous(data) {
+	var out =
+		'"' +
+		getProperty(data, "representant.prenom", "") +
+		" " +
+		getProperty(data, "representant.nom", "") +
+		'" <' +
+		getProperty(data, "representant.email", "") +
+		">";
+	return out;
+};
 
 /**
  * Front matter bcc
  * @param {Object} data
  * @return {String}
  */
-export const bcc = function anonymous(data
-) {
-var out='zipang <christophe.desguez@gmail.com>';return out;
-}
-
+export const bcc = function anonymous(data) {
+	var out = "zipang <christophe.desguez@gmail.com>";
+	return out;
+};
 
 /**
  * Front matter attachments
  * @param {Object} data
  * @return {String}
  */
-export const attachments = function anonymous(data
-) {
-var out='attestation';return out;
-}
-
-
+export const attachments = function anonymous(data) {
+	var out = "attestation";
+	return out;
+};
 
 const welcome = {
 	text,
-	html,subject,
+	html,
+	subject,
 	to,
 	bcc,
 	attachments
