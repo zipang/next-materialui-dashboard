270fc167fbc5eefcf1d098a7d161e2ea0cee7c43
zipang <christophe.Desguez@gmail.com>
2021-03-18 09:42:20 +0100

    Restore previous Wizard state when the last siret search from the localStorage correspond to the current user

--- src/components/forms/registration/RegistrationWizard.js
+++ src/components/forms/registration/RegistrationWizard.js
@@ -1,6 +1,7 @@
 import { useAuthentication } from "@components/AuthenticationProvider";
 import Wizard from "@forms/wizard/Wizard";
 import AdherentsApiClient from "@lib/client/AdherentsApiClient";
+import { useEffect, useState } from "react";
 import {
 	welcomeStepsNewAccount,
 	siretSearch,
@@ -19,27 +20,48 @@ const steps = [
 
 /**
  * Look if we have an incomplete registration process in the local storage
- * Or create
  */
-const restoreState = (loggedUser) => {
+const restorePreviousState = async (loggedUser) => {
 	try {
 		const siret = window.localStorage.getItem("siret-search");
-		const { adherent } = AdherentsApiClient.retrieveBySiret(siret);
-		if (adherent && adherent.owner === loggedUser.username) {
-			console.log(
-				`Restoring previous registration state from localStorage: ${adherent.nom} (${adherent.statut})`
-			);
-			return { data: adherent };
+		if (siret) {
+			const { adherent } = await AdherentsApiClient.retrieveBySiret(siret);
+			if (adherent && adherent.owner === loggedUser.username) {
+				console.log(
+					`Restoring previous registration state from localStorage: ${adherent.nom} (${adherent.statut})`
+				);
+				return { data: adherent, initialStep: adherent.statut };
+			}
 		}
 	} catch (err) {
+		console.error(err);
 		window.localStorage.removeItem("siret-search");
 	}
+	// Nothing retrieved
+	return {};
 };
 
+/**
+ * Remove the Siret Search step when we are restoring the wizard to a pre-filled step
+ */
+const filterSteps = (steps, { initialStep }) =>
+	initialStep ? steps.filter((s) => s.id !== "step-siret-search") : steps;
+
 const RegistrationWizard = () => {
-	const loggedUser = useAuthentication();
-	const data = restoreState(loggedUser);
-	return <Wizard id="registration-wizard" data={data} steps={steps} />;
+	const { loggedUser } = useAuthentication();
+	const [props, setProps] = useState();
+
+	useEffect(async () => {
+		const restoredProps = await restorePreviousState(loggedUser);
+		if (restoredProps.initialStep) {
+			console.log(`Restored wizard state`, restoredProps);
+		}
+		setProps(restoredProps);
+	}, [false]);
+
+	return props ? (
+		<Wizard id="registration-wizard" {...props} steps={filterSteps(steps, props)} />
+	) : null;
 };
 
 export default RegistrationWizard;
