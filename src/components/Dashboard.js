2d082904373c851c0cd598d5e181f6e218478a47
zipang <christophe.Desguez@gmail.com>
2021-03-29 13:58:25 +0200

    Move the dashboard (full page template) as a component to re-use it more easily across pages

---
+++ src/components/Dashboard.js
@@ -0,0 +1,67 @@
+import { makeStyles } from "@material-ui/core/styles";
+import Header from "@components/Header";
+import UserNavBar from "@components/navigation/NavigationBar";
+import Copyright from "@components/Copyright";
+import { useRouter } from "next/router";
+import { useAuthentication } from "./AuthenticationProvider";
+
+const drawerWidth = 256;
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		display: "flex",
+		minHeight: "100vh"
+	},
+	drawer: {
+		[theme.breakpoints.up("sm")]: {
+			width: drawerWidth,
+			flexShrink: 0
+		}
+	},
+	app: {
+		flex: 1,
+		display: "flex",
+		flexDirection: "column",
+		overflow: "hidden"
+	},
+	main: {
+		flex: 1,
+		padding: theme.spacing(6, 4),
+		background: "#eaeff1",
+		"& .action": {
+			// The floating action button
+			position: "absolute",
+			right: "2rem",
+			bottom: "4rem"
+		}
+	},
+	footer: {
+		padding: theme.spacing(2),
+		background: "#eaeff1"
+	}
+}));
+
+const Dashboard = ({ title, tabsDefs = [], currentTab, children }) => {
+	const classes = useStyles();
+	const router = useRouter();
+	const { loggedUser } = useAuthentication();
+
+	return loggedUser ? (
+		<div className={classes.root}>
+			<nav className={classes.drawer}>
+				<UserNavBar user={loggedUser} selectedNav={router.pathname} />
+			</nav>
+			<div className={classes.app}>
+				<Header title={title} tabsDefs={tabsDefs} currentTab={currentTab} />
+				<main className={classes.main}>{children}</main>
+				<footer className={classes.footer}>
+					<Copyright />
+				</footer>
+			</div>
+		</div>
+	) : (
+		<strong>Forbidden. You must be logged to see this page.</strong>
+	);
+};
+
+export default Dashboard;
---
+++ src/components/DetailAdherentDashboard.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import { useEventBus } from "@components/EventBusProvider.js";
+import ActionButton from "@components/forms/ActionButton.js";
+import TabbedView, { buildTabHeaders } from "@components/forms/tabs/TabbedView.js";
+import AdherentsApiClient from "@lib/client/AdherentsApiClient.js";
+import { formSteps } from "@forms/registration/RegistrationSteps.js";
+import Dashboard from "@components/Dashboard.js";
+import { useAuthentication } from "./AuthenticationProvider";
+
+// Some steps may not contain fields
+const steps = formSteps.filter((step) => Array.isArray(step.fields));
+
+/**
+ * Display the detail of an adherent using multiple tabs
+ * to organize the data
+ */
+export const DetailAdherentDashboard = ({ siret }) => {
+	const eb = useEventBus();
+	const { loggedUser } = useAuthentication();
+	const [currentTab, setCurrentTab] = useState(formSteps[0].id);
+	const [editMode, setEditMode] = useState(false);
+	const [adherent, setAdherent] = useState();
+	const [error, setError] = useState(false);
+
+	const updateAdherent = async (data) => {
+		setAdherent(await AdherentsApiClient.update(loggedUser, data));
+		setEditMode(false);
+	};
+
+	useEffect(async () => {
+		try {
+			const adherent = await AdherentsApiClient.retrieveBySiret(siret);
+			setAdherent(adherent);
+		} catch (err) {
+			setError(err.message);
+		}
+	}, [false]);
+
+	useEffect(() => {
+		const tabSubmit = `${currentTab}:submit`;
+		eb.on(tabSubmit, updateAdherent);
+		return () => {
+			eb.off(tabSubmit, updateAdherent);
+		};
+	}, [siret, currentTab]);
+
+	return (
+		<Dashboard
+			title={adherent && adherent.nom}
+			tabsDefs={buildTabHeaders(steps, eb, setCurrentTab)}
+			currentTab={currentTab}
+		>
+			<TabbedView steps={steps} data={adherent} error={error} editMode={editMode} />
+			{editMode ? (
+				<ActionButton
+					label="Enregistrer"
+					name="Save"
+					onClick={() => eb.emit(`${currentTab}:validate`)}
+				/>
+			) : (
+				<ActionButton
+					label="Editer"
+					name="Edit"
+					onClick={() => setEditMode(true)}
+				/>
+			)}
+		</Dashboard>
+	);
+};
+
+export default DetailAdherentDashboard;
