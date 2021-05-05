6f6ca00cb07627d1b6cbff01d8a8b78db636c6d9
zipang <christophe.Desguez@gmail.com>
2021-05-05 21:59:34 +0200

    Adhésions à renouveler et à clore

--- src/models/Adhesion.js
+++ src/models/Adhesion.js
@@ -3,6 +3,11 @@ import { getParseInstance } from "./ParseSDK.js";
 import { getNextAdhesionNumber, updateAdhesionNumber } from "./Parameters.js";
 import { retrieveBySiret } from "./Adherent.js";
 import ApiError from "../lib/ApiError.js";
+import { sendMailTemplate } from "../lib/client/MailApiClient.js";
+import { formatISODate } from "../components/forms/validation/utils.js";
+
+const formatDate = (d) => d.toISOString().substr(0, 10); // YYYY-MM-DD
+const frenchDate = formatISODate("dd/mm/yyyy");
 
 // Extract these fields only when requesting a list
 const _DEFAULT_FIELD_LIST = [
@@ -91,6 +96,71 @@ export const retrieve = async (params = {}, fields = _DEFAULT_FIELD_LIST) => {
 export const retrieveByNo = async (no) => {
 	const Parse = getParseInstance(); // Because this instance has been augmented with new utility methods
 	return Parse.retrieveByUniqueKey("Adhesion", "no", no);
+};
+
+/**
+ * List and flag adhesions to renew
+ */
+export const toRenew = async () => {
+	const today = new Date();
+	const inAMonth = new Date();
+	inAMonth.setMonth(today.getMonth() + 1);
+
+	// Look for all the adhesions that will expire in a month
+	const Parse = await getParseInstance();
+	const query = new Parse.Query("Adhesion");
+	query.include("questioning");
+	query.select(["no", "nom", "statut", "date_fin", "adherent.representant"]);
+	query.lessThanOrEqualTo("date_fin", formatDate(inAMonth));
+	query.notEqualTo("statut", "closed"); // do not loop over already expired adhesions
+
+	const closingAdhesions = await query.findAll();
+	const report = {
+		a_renouveler: [],
+		closed: []
+	};
+
+	await Promise.all(
+		closingAdhesions.map(async (adhesion) => {
+			const statut = adhesion.get("statut");
+			const date_fin = adhesion.get("date_fin");
+			const adherent = adhesion.get("adherent");
+			console.log(
+				`Adhesion ${adhesion.get(
+					"no"
+				)} will expire at ${date_fin} when today is ${formatDate(today)}`
+			);
+
+			const sendReminderEmail = async () => {
+				const data = adhesion.toJSON();
+				data.today = frenchDate(formatDate(today));
+				data.date_fin = frenchDate(data.date_fin);
+				data.adherent = adherent.toJSON();
+				await sendMailTemplate("a_renouveler", data);
+			};
+
+			if (date_fin < formatDate(today)) {
+				// This time it's over
+				adhesion.set("statut", "closed");
+				adherent.set("statut", "inactif");
+				await Promise.all([adherent.save(), adhesion.save()]);
+				// Do it again
+				sendReminderEmail();
+				report.closed.push(`${adhesion.get("no")} - ${adhesion.get("nom")}`);
+			} else if (statut === "active") {
+				// Mark the adhesion
+				adhesion.set("statut", "a_renouveler");
+				await adhesion.save();
+				// Send the reminder email
+				sendReminderEmail();
+				report.a_renouveler.push(
+					`${adhesion.get("no")} - ${adhesion.get("nom")}`
+				);
+			}
+			return true; //
+		})
+	);
+	return report;
 };
 
 /**
--- src/models/Adhesion.spec.js
+++ src/models/Adhesion.spec.js
@@ -1,6 +1,10 @@
 import suite from "baretest";
 import { getParseInstance } from "./ParseSDK.js";
-import { create as createAdhesion, retrieve as retrieveAdhesions } from "./Adhesion.js";
+import {
+	create as createAdhesion,
+	retrieve as retrieveAdhesions,
+	toRenew
+} from "./Adhesion.js";
 import code from "@hapi/code";
 
 const { expect } = code;
@@ -16,23 +20,32 @@ AdhesionTestSuite.before(() => {
 	Parse = getParseInstance();
 });
 
-AdhesionTestSuite("Create a new Adhesion", async () => {
-	const adh = await createAdhesion(testAdherent.siret, {
-		mode_paiement: "cheque"
-	});
+// AdhesionTestSuite("Create a new Adhesion", async () => {
+// 	const adh = await createAdhesion(testAdherent.siret, {
+// 		mode_paiement: "cheque"
+// 	});
 
-	console.log(adh); // It is a Parse Object
-	expect(adh.get("no")).to.be.a.string();
-	expect(adh.get("statut")).to.equal("en_attente");
-	expect(adh.get("mode_paiement")).to.equal("cheque");
-});
+// 	console.log(adh); // It is a Parse Object
+// 	expect(adh.get("no")).to.be.a.string();
+// 	expect(adh.get("statut")).to.equal("en_attente");
+// 	expect(adh.get("mode_paiement")).to.equal("cheque");
+// });
 
-AdhesionTestSuite("Retrieve current adhesions", async () => {
-	const adhesions = await retrieveAdhesions();
-	console.log(`Retrieved adhesions`, adhesions);
+// AdhesionTestSuite("Retrieve current adhesions", async () => {
+// 	const adhesions = await retrieveAdhesions();
+// 	console.log(`Retrieved adhesions`, adhesions);
 
-	expect(adhesions).to.be.an.array();
-	adhesions.forEach((adh) => expect(adh.no.length).to.equal(8));
+// 	expect(adhesions).to.be.an.array();
+// 	adhesions.forEach((adh) => expect(adh.no.length).to.equal(8));
+// });
+
+AdhesionTestSuite("List adhesions to renew", async () => {
+	const adhesions = await toRenew();
+	console.log(`Adhesions to renew`, adhesions);
+
+	expect(adhesions).to.be.an.object();
+	expect(adhesions.a_renouveler).to.be.an.array();
+	expect(adhesions.closed).to.be.an.array();
 });
 
 export default AdhesionTestSuite;
