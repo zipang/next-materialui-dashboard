d5ad93876a8bdc40a4129df1f62666cc307afbe7
zipang <christophe.Desguez@gmail.com>
2021-05-07 16:49:09 +0200

    Add the abilioty to pass a function as response. The function is evaluated after ms

--- src/lib/utils/Promises.js
+++ src/lib/utils/Promises.js
@@ -1,8 +1,14 @@
 /**
- * Build a promise that resolve in `ms` milliseconds with the data provided
+ * Build a promise that resolve in `ms` milliseconds with the response provided
+ * NOTE: response may be a function, whose evaluation would be delayed by ms
  * @param {Number} ms The delay to wait before resolving the Promise
- * @param {Any} data The data to return after ms
+ * @param {Any} response The data to return after ms.
  * @return {Promise}
  */
-export const delay = (ms, data) =>
-	new Promise((resolve) => setTimeout(() => resolve(data), ms));
+export const delay = (ms, response) =>
+	new Promise((resolve) =>
+		setTimeout(
+			() => resolve(typeof response === "function" ? response() : response),
+			ms
+		)
+	);
